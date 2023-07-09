b7053eb1ae92c094ea899172bb1886c45cd9f4a8
Muhammad Sarmad <muhammadsarmad24@gmail.com>
2023-07-10 00:18:31 +0500

    rename

--- packages/ui/Container/types.tsx
+++
@@ -1,7 +0,0 @@
-import { ReactNode } from "react";
-import { StyleProp, ViewStyle } from "react-native"
-
-export interface ContainerProps {
-    children: ReactNode;
-    style?:StyleProp<ViewStyle>
-}
