29febb968f68e6092d82e59fc55adb9d99e7274a
Muhammad Sarmad <muhammadsarmad24@gmail.com>
2023-07-10 20:56:07 +0500

    updated ID

--- apps/api/src/models/users.rs
+++ apps/api/src/models/users.rs
@@ -2,7 +2,8 @@ use serde::{Deserialize, Serialize};
 
 #[derive(Clone, Debug, Deserialize, Serialize)]
 pub struct User {
-    pub id: u32,
+    #[serde(rename = "_id", skip_serializing_if = "Option::is_none")]
+    pub id: Option<ObjectId>,
     pub first_name: String,
     pub last_name: String,
     pub email: String,
