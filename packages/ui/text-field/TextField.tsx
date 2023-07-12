2eabc84303951508bce7638abbf33ecc86466046
Hamza Mukhtar <hamzamukhtar292@gmail.com>
2023-07-12 20:57:58 +0500

    update

--- packages/ui/text-field/TextField.tsx
+++ packages/ui/text-field/TextField.tsx
@@ -1,10 +1,26 @@
 import { FC } from "react";
-import { View, TextInput, TextInputProps } from "react-native";
+import { TextInput, TextInputProps } from "react-native";
 
-export const TextFiled: FC<TextFieldType> = ({ style, ...props }) => {
-  return <TextInput style={[style]} {...props} />;
+const variants: Record<TextInputVariants, any> = {
+  outlined: {
+    borderColor: "gray",
+    width: "100%",
+    borderWidth: 1,
+    borderRadius: 10,
+    padding: 10,
+  },
+  filled: {
+    fontSize: 14,
+  },
+};
+
+export const TextFiled: FC<TextFieldType> = ({ variant="oultined", style, ...props }) => {
+  return <TextInput style={[[variants[variant],style]]} {...props} />;
 };
 
 type TextFieldType = TextInputProps & {
   children?: JSX.Element;
+  variant: TextInputVariants;
 };
+
+type TextInputVariants = "oultined" | "filled";
