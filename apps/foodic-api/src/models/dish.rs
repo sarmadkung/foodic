749a96d45f4cae93bc306c492bc3e69a47c9e468
Hamza Mukhtar <hamzamukhtar292@gmail.com>
2023-08-14 16:30:00 +0500

    updated dish

--- apps/foodic-api/src/models/dish.rs
+++ apps/foodic-api/src/models/dish.rs
@@ -6,8 +6,12 @@ pub struct Dish {
     #[serde(rename = "_id", skip_serializing_if = "Option::is_none")]
     pub id: Option<ObjectId>,
     pub name: String,
-    pub ingredients: Vec<String>,
-    pub dish_type: DishType,
+    pub main_ingredients: Vec<String>,
+    pub category: DishType,
+    pub description: String,
+    pub nutritional_info: String,
+    pub cooking_time: Number,
+    pub cooking_method: String,
 }
 
 #[derive(Clone, Debug, Serialize, Deserialize)]
