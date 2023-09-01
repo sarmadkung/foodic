9b0ad949566d71e3ecfe4d8417635563e3c9f56f
Muhammad Sarmad <muhammadsarmad24@gmail.com>
2023-09-01 17:57:14 +0500

    fix

--- apps/expo/app/index.tsx
+++ apps/expo/app/index.tsx
@@ -1,75 +1,113 @@
-import React, { useState } from "react";
-import { TouchableOpacity, View } from "react-native";
-import { Box, Label, Row, Text, TextField, RowImageText, Container } from "ui";
-import { Button } from "ui";
-import { TextInputIcon } from "ui";
-import { ThemeProvider } from "../components/theme";
-import { useTheme } from "../context";
+// import React, { useState } from "react";
+// import { TouchableOpacity, View } from "react-native";
+// import { Box, Label, Row, Text, TextField, RowImageText, Container } from "ui";
+// import { Button } from "ui";
+// import { TextInputIcon } from "ui";
+// import { ThemeProvider } from "../components/theme";
+// import { useTheme } from "../context";
 
-export default function Page() {
-  const { theme } = useTheme();
-  const handlePress = () => {
-    alert("im pressed");
-  };
+// export default function Page() {
+//   const { theme } = useTheme();
+//   const handlePress = () => {
+//     alert("im pressed");
+//   };
+//   return (
+//     <Container
+//       style={{
+//         alignItems: "center",
+//         justifyContent: "center",
+//         backgroundColor: theme.colors.base,
+//         flex: 1,
+//       }}
+//     >
+//       <Row>
+//         <Text variant="title" colorVariant="body">
+//           Welcome here at expo router
+//         </Text>
+//         <Button colorVariant="primary" variant="highlighted">
+//           <Text variant="title" colorVariant="body">
+//             ME
+//           </Text>
+//         </Button>
+//       </Row>
+//       <TextField
+//         variant="outlined"
+//         style={{ padding: 10, color: "red" }}
+//         placeholderTextColor="pink"
+//         placeholder="Enter your name"
+//       />
+//       <Box
+//         imageSource={require("../../../apps/expo/assets/dish.jpg")}
+//         title="Box Title"
+//         description="Box Description"
+//         onPress={handlePress}
+//       />
+//       <View>
+//         <Label variant="default">Default Label</Label>
+//         <Label variant="outlined">Outlined Label</Label>
+//         <Label variant="highlighted">Highlighted Label</Label>
+//       </View>
+//       <TextInputIcon
+//         variant="default"
+//         leftIcon="person"
+//         rightIcon="lock-closed"
+//         placeholder="Username"
+//       />
+//       <View>
+//         <RowImageText
+//           imageSource={require("../assets/dish.jpg")}
+//           title="Example Title"
+//           description="Example Description"
+//           variant="primary"
+//           price="$100"
+//         />
+
+//         <RowImageText
+//           imageSource={require("../assets/dish.jpg")}
+//           title="Example Title"
+//           description="Example Description"
+//           variant="secondary"
+//           price="$100"
+//         />
+//       </View>
+//     </Container>
+//   );
+// }
+
+import { useFonts } from "expo-font";
+
+import { StatusBar } from "expo-status-bar";
+
+import { useColorScheme } from "react-native";
+
+import { Paragraph, Spacer, TamaguiProvider, Theme, YStack } from "tamagui";
+import config from "../tamagui.config";
+export default function App() {
+  const colorScheme = useColorScheme();
+  const [loaded] = useFonts({
+    Inter: require("@tamagui/font-inter/otf/Inter-Medium.otf"),
+
+    InterBold: require("@tamagui/font-inter/otf/Inter-Bold.otf"),
+  });
+  if (!loaded) {
+    return null;
+  }
   return (
-    <Container
-      style={{
-        alignItems: "center",
-        justifyContent: "center",
-        backgroundColor: theme.colors.base,
-        flex: 1,
-      }}
-    >
-      <Row>
-        <Text variant="title" colorVariant="body">
-          Welcome here at expo router
-        </Text>
-        <Button colorVariant="primary" variant="highlighted">
-          <Text variant="title" colorVariant="body">
-            ME
-          </Text>
-        </Button>
-      </Row>
-      <TextField
-        variant="outlined"
-        style={{ padding: 10, color: "red" }}
-        placeholderTextColor="pink"
-        placeholder="Enter your name"
-      />
-      <Box
-        imageSource={require("../../../apps/expo/assets/dish.jpg")}
-        title="Box Title"
-        description="Box Description"
-        onPress={handlePress}
-      />
-      <View>
-        <Label variant="default">Default Label</Label>
-        <Label variant="outlined">Outlined Label</Label>
-        <Label variant="highlighted">Highlighted Label</Label>
-      </View>
-      <TextInputIcon
-        variant="default"
-        leftIcon="person"
-        rightIcon="lock-closed"
-        placeholder="Username"
-      />
-      <View>
-        <RowImageText
-          imageSource={require("../assets/dish.jpg")}
-          title="Example Title"
-          description="Example Description"
-          variant="primary"
-          price="$100"
-        />
+    <TamaguiProvider config={config}>
+      <Theme name={colorScheme === "dark" ? "dark" : "light"}>
+        <YStack
+          f={1}
+          jc="center"
+          ai="center"
+          backgroundColor={"$backgroundSoft"}
+        >
+          <Paragraph color="$color" jc="center">
+            {colorScheme}
+          </Paragraph>
 
-        <RowImageText
-          imageSource={require("../assets/dish.jpg")}
-          title="Example Title"
-          description="Example Description"
-          variant="secondary"
-          price="$100"
-        />
-      </View>
-    </Container>
+          <StatusBar style="auto" />
+        </YStack>
+      </Theme>
+    </TamaguiProvider>
   );
 }
--- apps/expo/babel.config.js
+++ apps/expo/babel.config.js
@@ -1,8 +1,24 @@
+process.env.TAMAGUI_TARGET = "native";
 module.exports = function (api) {
   api.cache(true);
   return {
     presets: ["babel-preset-expo"],
     plugins: [
+      [
+        "transform-inline-environment-variables",
+        // NOTE: include is optional, you can leave this part out
+        {
+          include: ["TAMAGUI_TARGET", "EXPO_ROUTER_APP_ROOT"],
+        },
+      ],
+      [
+        "@tamagui/babel-plugin",
+        {
+          components: ["tamagui"],
+          config: "./tamagui.config.ts",
+          logTimings: true,
+        },
+      ],
       "react-native-reanimated/plugin",
       "@babel/plugin-transform-export-namespace-from",
       require.resolve("expo-router/babel"),
